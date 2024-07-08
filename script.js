4b6e2082362f3b0425c4a24478491ccd7cc2be80
Chowiee <165966007+Chiwiee@users.noreply.github.com>
2024-07-09 06:18:54 +0800

    Create span querySelector

--- script.js
+++ script.js
@@ -38,6 +38,7 @@ function displayTextContent() {
   let buttons = document.querySelectorAll("button");
   buttons.forEach((button) => {
     button.addEventListener("click", () => {
+      let content = document.querySelector("span");
       switch (button.id) {
         case "num0":
           break;
