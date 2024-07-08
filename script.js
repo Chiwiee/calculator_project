fed026ca3a9a7ec72d70f28cbf7a747e150750b6
Chowiee <165966007+Chiwiee@users.noreply.github.com>
2024-07-09 05:30:52 +0800

    Create switch case statement

--- script.js
+++ script.js
@@ -38,7 +38,42 @@ function displayTextContent() {
   let buttons = document.querySelectorAll("button");
   buttons.forEach((button) => {
     button.addEventListener("click", () => {
-      console.log(button.id);
+      switch (button.id) {
+        case "num0":
+          break;
+        case "num1":
+          break;
+        case "num2":
+          break;
+        case "num3":
+          break;
+        case "num4":
+          break;
+        case "num5":
+          break;
+        case "num6":
+          break;
+        case "num7":
+          break;
+        case "num8":
+          break;
+        case "num9":
+          break;
+        case "num0":
+          break;
+        case "clear":
+          break;
+        case "plus":
+          break;
+        case "minus":
+          break;
+        case "times":
+          break;
+        case "divide":
+          break;
+        case "equal":
+          break;
+      }
     });
   });
 }
