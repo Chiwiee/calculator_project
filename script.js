e99502e08c4bb03a6d7fc1b8bd918f9427c3013a
Chowiee <165966007+Chiwiee@users.noreply.github.com>
2024-07-09 05:22:31 +0800

    Move console.log inside operate function

--- script.js
+++ script.js
@@ -11,14 +11,14 @@ function divide(firstNumber, secondNumber) {
   return firstNumber / secondNumber;
 }
 
-// console.log(firstNumber);
-// console.log(operator);
-// console.log(secondNumber);
-
 function operate(firstNumber, operator, secondNumber) {
   const firstNumber = prompt("Enter a Number");
   const operator = prompt("Operator: +, -, *, /");
   const secondNumber = prompt("Enter a Number");
+
+  console.log(firstNumber);
+  console.log(operator);
+  console.log(secondNumber);
 
   switch (operator) {
     case "+":
